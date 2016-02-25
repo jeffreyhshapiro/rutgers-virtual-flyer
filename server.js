b059e23ac9da2e21f335041413382e6311b39f26
C-Das <chinmay98@gmail.com>
2016-02-25 16:26:31 -0500

    package.json file included and all packages are included

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var Sequelize = require('sequelize');
+var bodyParser = require('body-parser');
+var PORT = process.env.NODE_ENV || 8080;
+
+var app = express();
+var connection = new Sequelize('DB_Virtual_Flyer','root');
+
+app.engine('handlebars',expressHandlebars({
+  defaultLayout :'main'
+}));
+app.set('view engine','handlebars');
+
+app.use(bodyParser.urlencoded({
+  extended :false
+}));
+
+app.get('/',function(req,res){
+  res.send("Hello there !!");
+});
+
+connection.sync().then(function(){
+  app.listen(PORT,function(){
+    console.log("Application is listening on PORT %s",PORT);
+  });
+});
