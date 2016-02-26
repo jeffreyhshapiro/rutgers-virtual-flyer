44b6516ac5f2e6e0d4fdcfd4a24895803ac4c271
C-Das <chinmay98@gmail.com>
2016-02-25 20:46:29 -0500

    updated PORT env to deploy to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var expressHandlebars = require('express-handlebars');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
-var PORT = process.env.NODE_ENV || 8080;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 var connection = new Sequelize('DB_Virtual_Flyer','root');
