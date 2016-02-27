ece9c0db65b2fe4a84f85ea6366b97ad6486707a
C-Das <chinmay98@gmail.com>
2016-02-27 13:32:21 -0500

    Added the portion to register

--- public/dbscript.sql
+++ public/dbscript.sql
@@ -5,6 +5,8 @@ USE DB_Virtual_Flyer;
 CREATE TABLE login (
 id int AUTO_INCREMENT,
 eMail varchar(100) NOT NULL,
+fName varchar(255) NOT NULL,
+lName varchar(255) NOT NULL,
 password varchar(255) NOT NULL,
 active boolean,
 PRIMARY KEY(id)
--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,45 @@
-// var server  = require('./server.js');
+
+window.userInfo = {
+    eMail : "",
+    fName : "",
+    lName : "",
+    userExists: "False"
+};
 
 $(document).ready(function(){
 
    $("#loginModal").modal("show");
+
+   $("#email").on("click",function(){
+     $("#loginModal").modal("hide");
+     $("#emailModal").modal("show");
+
+     $("#saveRegister").on("click",function(e){
+        e.preventDefault();
+          //var fName,lName,eMail;
+
+            req.send("Test save click");
+
+            // userInfo.fName = $("#firstName").val();
+            // userInfo.lName = $("#lastName").val();
+            // userInfo.eMail = $("#eMail").val();
+
+            console.log("User Info:"+req.body.fName);
+            console.log("User Info:"+req.body.lName);
+            console.log("User Info:"+req.body.eMail);
+
+            // connection.child(userInfo.fName).set({
+            //   eMail : userInfo.eMail,
+            //   fName : userInfo.fName,
+            //   lName : userInfo.lName,
+            //   passord: test
+            //   active :true;
+            // });
+            // $("#userInputForm").hide();
+            // $("#SuccessfulLogin").show();
+            // $("#validationMessage").html("Congratulations "+userInfo.fName+", your account has been successfully created.").css("color","black")
+            //  .append("Click 'Start Search' to start your search").css("color","black");
+      });
+   });
 
 });
